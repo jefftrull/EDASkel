cbb5b1f9c30a47b22c4e5a8e741206d7f0b0f8cb
Jeff Trull <edaskel@att.net>
2013-11-25 21:05:25 -0800

    SPEF: refactor tests to reduce code duplication

--- parser/tests/spefbasic.cpp
+++ parser/tests/spefbasic.cpp
@@ -34,24 +34,24 @@ struct Visitor {
     names.push_back(n);
     return id;   // just the offset within the name map, for now
   }
-  typedef std::pair<net_token_value_t, char> port_t;
+
   void port_definition(net_token_value_t net, char dir) {
     ports.emplace_back(net, dir);
   }
 
   std::vector<std::string> names;
+  typedef std::pair<net_token_value_t, char> port_t;
   std::vector<port_t> ports;
 };
 
 // without this we would have to make std::pair<size_t, char> iostream-able
 BOOST_TEST_DONT_PRINT_LOG_VALUE(Visitor::port_t);
 
-Visitor spefVisitor;
-spefparser<SpefIter, Visitor> spefParser(spefVisitor);
-spefskipper<SpefIter> spefSkipper;
-
 // boilerplate parsing code
-void parse_check(std::string const& str, spef& result) {
+void parse_check(std::string const& str, Visitor& spefVisitor, spef& result) {
+  spefparser<SpefIter, Visitor> spefParser(spefVisitor);
+  spefskipper<SpefIter> spefSkipper;
+
   std::stringstream testspef(str);
   testspef.unsetf(std::ios::skipws);
   SpefIter beg(testspef), end;
@@ -59,8 +59,17 @@ void parse_check(std::string const& str, spef& result) {
   BOOST_CHECK( (beg == end) );                        // we should consume all input
 }
 
+// for checks not needing data from the visitor
+void parse_check(std::string const& str, spef& result) {
+  Visitor spefVisitor;
+  parse_check(str, spefVisitor, result);
+}
 
 void parse_check_fail(std::string const& str) {
+  Visitor spefVisitor;
+  spefparser<SpefIter, Visitor> spefParser(spefVisitor);
+  spefskipper<SpefIter> spefSkipper;
+
   std::stringstream testspef(str);
   testspef.unsetf(std::ios::skipws);
   SpefIter beg(testspef), end;
@@ -68,130 +77,112 @@ void parse_check_fail(std::string const& str) {
   BOOST_CHECK( !phrase_parse(beg, end, spefParser, spefSkipper, result) );  // we should NOT match
 }
 
+namespace spefData {
+  std::string preamble("SPEF\n*SPEF \"IEEE 1481-1998\"\n*DESIGN \"GreatDesign\"\n"
+                       "*DATE \"Fri Aug 29 02:14:00 1997\"\n*VENDOR \"MegaEDA\"\n"
+                       "*PROGRAM \"Ozymandius Extractor\"\n*VERSION \"0.99\"\n");
+  std::string empty_flow("*DESIGN_FLOW\n");
+  std::string delimiters("*DIVIDER /\n*DELIMITER :\n*BUS_DELIMITER [ ]\n");
+  std::string units("*T_UNIT 1 PS\n*C_UNIT 1 PF\n*R_UNIT 1 KOHM\n*L_UNIT 1 HENRY\n");
+  std::string header(preamble + empty_flow + delimiters + units);
+
+}
+
 BOOST_AUTO_TEST_CASE( header ) {
 
-   spef result;
-   std::string units("*T_UNIT 1 PS\n*C_UNIT 1 PF\n*R_UNIT 1 KOHM\n*L_UNIT 1 HENRY\n");
-   std::string preamble("SPEF\n*SPEF \"IEEE 1481-1998\"\n*DESIGN \"GreatDesign\"\n");
-   preamble += "*DATE \"Fri Aug 29 02:14:00 1997\"\n*VENDOR \"MegaEDA\"\n";
-   preamble += "*PROGRAM \"Ozymandius Extractor\"\n*VERSION \"0.99\"\n";
-   std::string empty_flow("*DESIGN_FLOW\n");
-   std::string delimiters("*DIVIDER /\n*DELIMITER :\n*BUS_DELIMITER [ ]\n");
+  spef result;
+  using namespace spefData;
 
-   parse_check(preamble + "// some other comment\n" + empty_flow + delimiters + units, result);
-   BOOST_CHECK_EQUAL( "GreatDesign", result.name );
-   BOOST_CHECK_EQUAL( "IEEE 1481-1998", result.standard );
-   BOOST_CHECK_EQUAL( "MegaEDA", result.vendor );
-   BOOST_CHECK_EQUAL( "Ozymandius Extractor", result.program );
-   BOOST_CHECK_EQUAL( "0.99", result.version );
-   BOOST_CHECK(       result.design_flow.empty() );
+  parse_check(preamble + "// some other comment\n" + empty_flow + delimiters + spefData::units, result);
+  BOOST_CHECK_EQUAL( "GreatDesign", result.name );
+  BOOST_CHECK_EQUAL( "IEEE 1481-1998", result.standard );
+  BOOST_CHECK_EQUAL( "MegaEDA", result.vendor );
+  BOOST_CHECK_EQUAL( "Ozymandius Extractor", result.program );
+  BOOST_CHECK_EQUAL( "0.99", result.version );
+  BOOST_CHECK(       result.design_flow.empty() );
 
-   std::string more_interesting_flow("*DESIGN_FLOW \"ALPHA BETA_GAMMA\" \"DELTA\"\n");
-   parse_check(preamble + more_interesting_flow + delimiters + units, result);
-   BOOST_CHECK_EQUAL( 2, result.design_flow.size() );
-   BOOST_CHECK_EQUAL( "BETA_GAMMA", result.design_flow.find("ALPHA")->second );
-   BOOST_CHECK_EQUAL( "", result.design_flow.find("DELTA")->second );
+  std::string more_interesting_flow("*DESIGN_FLOW \"ALPHA BETA_GAMMA\" \"DELTA\"\n");
+  parse_check(preamble + more_interesting_flow + delimiters + units, result);
+  BOOST_CHECK_EQUAL( 2, result.design_flow.size() );
+  BOOST_CHECK_EQUAL( "BETA_GAMMA", result.design_flow.find("ALPHA")->second );
+  BOOST_CHECK_EQUAL( "", result.design_flow.find("DELTA")->second );
 
 }
 
 BOOST_AUTO_TEST_CASE( units ) {
 
-   spef result;
-   std::string preamble("SPEF\n*SPEF \"IEEE 1481-1998\"\n*DESIGN \"GreatDesign\"\n");
-   preamble += "*DATE \"Fri Aug 29 02:14:00 1997\"\n*VENDOR \"MegaEDA\"\n";
-   preamble += "*PROGRAM \"Ozymandius Extractor\"\n*VERSION \"0.99\"\n*DESIGN_FLOW\n";
-   preamble += "*DIVIDER /\n*DELIMITER :\n*BUS_DELIMITER [ ]\n";
-   parse_check(preamble + "*T_UNIT 1 PS\n*C_UNIT 1 PF\n*R_UNIT 1 KOHM\n*L_UNIT 1 HENRY\n", result);
-   BOOST_CHECK_EQUAL( (quantity<si::time, double>(1e-12 * si::seconds)), result.t_unit );
-   BOOST_CHECK_EQUAL( (quantity<si::resistance, double>(1e3 * si::ohms)), result.r_unit );
-   BOOST_CHECK_EQUAL( (quantity<si::capacitance, double>(1e-12 * si::farads)), result.c_unit );
-   BOOST_CHECK_EQUAL( (quantity<si::inductance, double>(1 * si::henrys)), result.l_unit );
+  spef result;
+  std::string preamble = spefData::preamble + spefData::empty_flow + spefData::delimiters;
+  parse_check(preamble + "*T_UNIT 1 PS\n*C_UNIT 1 PF\n*R_UNIT 1 KOHM\n*L_UNIT 1 HENRY\n", result);
+  BOOST_CHECK_EQUAL( (quantity<si::time, double>(1e-12 * si::seconds)), result.t_unit );
+  BOOST_CHECK_EQUAL( (quantity<si::resistance, double>(1e3 * si::ohms)), result.r_unit );
+  BOOST_CHECK_EQUAL( (quantity<si::capacitance, double>(1e-12 * si::farads)), result.c_unit );
+  BOOST_CHECK_EQUAL( (quantity<si::inductance, double>(1 * si::henrys)), result.l_unit );
 
-   parse_check(preamble + "*T_UNIT 1 NS\n*C_UNIT 1 FF\n*R_UNIT 1 KOHM\n*L_UNIT 1 UH\n", result);
-   BOOST_CHECK_EQUAL( (quantity<si::time, double>(1e-9 * si::seconds)), result.t_unit );
-   BOOST_CHECK_EQUAL( (quantity<si::resistance, double>(1e3 * si::ohms)), result.r_unit );
-   BOOST_CHECK_EQUAL( (quantity<si::capacitance, double>(1e-15 * si::farads)), result.c_unit );
-   BOOST_CHECK_EQUAL( (quantity<si::inductance, double>(1e-6 * si::henrys)), result.l_unit );
+  parse_check(preamble + "*T_UNIT 1 NS\n*C_UNIT 1 FF\n*R_UNIT 1 KOHM\n*L_UNIT 1 UH\n", result);
+  BOOST_CHECK_EQUAL( (quantity<si::time, double>(1e-9 * si::seconds)), result.t_unit );
+  BOOST_CHECK_EQUAL( (quantity<si::resistance, double>(1e3 * si::ohms)), result.r_unit );
+  BOOST_CHECK_EQUAL( (quantity<si::capacitance, double>(1e-15 * si::farads)), result.c_unit );
+  BOOST_CHECK_EQUAL( (quantity<si::inductance, double>(1e-6 * si::henrys)), result.l_unit );
 
-   parse_check(preamble + "*T_UNIT 1 US\n*C_UNIT 1 PF\n*R_UNIT 1 OHM\n*L_UNIT 1 HENRY\n", result);
-   BOOST_CHECK_EQUAL( (quantity<si::time, double>(1e-6 * si::seconds)), result.t_unit );
-   BOOST_CHECK_EQUAL( (quantity<si::resistance, double>(1 * si::ohms)), result.r_unit );
+  parse_check(preamble + "*T_UNIT 1 US\n*C_UNIT 1 PF\n*R_UNIT 1 OHM\n*L_UNIT 1 HENRY\n", result);
+  BOOST_CHECK_EQUAL( (quantity<si::time, double>(1e-6 * si::seconds)), result.t_unit );
+  BOOST_CHECK_EQUAL( (quantity<si::resistance, double>(1 * si::ohms)), result.r_unit );
 
-   parse_check(preamble + "*T_UNIT 1 MS\n*C_UNIT 2.0 PF\n*R_UNIT 1.0 KOHM\n*L_UNIT 1 HENRY\n", result);
-   BOOST_CHECK_EQUAL( (quantity<si::time, double>(1e-3 * si::seconds)), result.t_unit );
-   BOOST_CHECK_EQUAL( (quantity<si::capacitance, double>(2e-12 * si::farads)), result.c_unit );
+  parse_check(preamble + "*T_UNIT 1 MS\n*C_UNIT 2.0 PF\n*R_UNIT 1.0 KOHM\n*L_UNIT 1 HENRY\n", result);
+  BOOST_CHECK_EQUAL( (quantity<si::time, double>(1e-3 * si::seconds)), result.t_unit );
+  BOOST_CHECK_EQUAL( (quantity<si::capacitance, double>(2e-12 * si::farads)), result.c_unit );
 
-   std::string crl("*C_UNIT 2.0 PF\n*R_UNIT 1.0 KOHM\n*L_UNIT 1 HENRY\n");
-   parse_check_fail(preamble + "*T_UNIT 1 QS\n" + crl);  // a fake unit prefix
-   parse_check_fail(preamble + "*X_UNIT 1 PS\n" + crl);  // no such thing as an "X" unit
-   parse_check_fail(preamble + "*T_UNIT 1 PF\n" + crl);  // time but I've given capacitance
+  std::string crl("*C_UNIT 2.0 PF\n*R_UNIT 1.0 KOHM\n*L_UNIT 1 HENRY\n");
+  parse_check_fail(preamble + "*T_UNIT 1 QS\n" + crl);  // a fake unit prefix
+  parse_check_fail(preamble + "*X_UNIT 1 PS\n" + crl);  // no such thing as an "X" unit
+  parse_check_fail(preamble + "*T_UNIT 1 PF\n" + crl);  // time but I've given capacitance
 
-   // quantity and unit mismatches
-   parse_check_fail(preamble + "*T_UNIT 1.0 PS\n*C_UNIT 1.0 PF\n*R_UNIT 1.0 FF\n*L_UNIT 1 HENRY\n");
-   parse_check_fail(preamble + "*T_UNIT 1.0 PS\n*C_UNIT 1.0 OHM\n*R_UNIT 1.0 KOHM\n*L_UNIT 1 HENRY\n");
+  // quantity and unit mismatches
+  parse_check_fail(preamble + "*T_UNIT 1.0 PS\n*C_UNIT 1.0 PF\n*R_UNIT 1.0 FF\n*L_UNIT 1 HENRY\n");
+  parse_check_fail(preamble + "*T_UNIT 1.0 PS\n*C_UNIT 1.0 OHM\n*R_UNIT 1.0 KOHM\n*L_UNIT 1 HENRY\n");
 
-   // TODO: test for missing spaces? Not presently checked.
+  // TODO: test for missing spaces? Not presently checked.
 }
 
 BOOST_AUTO_TEST_CASE( name_map ) {
 
-   spef result;
-   std::string units("*T_UNIT 1 PS\n*C_UNIT 1 PF\n*R_UNIT 1 KOHM\n*L_UNIT 1 HENRY\n");
-   std::string preamble("SPEF\n*SPEF \"IEEE 1481-1998\"\n*DESIGN \"GreatDesign\"\n");
-   preamble += "*DATE \"Fri Aug 29 02:14:00 1997\"\n*VENDOR \"MegaEDA\"\n";
-   preamble += "*PROGRAM \"Ozymandius Extractor\"\n*VERSION \"0.99\"\n";
-   std::string empty_flow("*DESIGN_FLOW\n");
-   std::string delimiters("*DIVIDER /\n*DELIMITER :\n*BUS_DELIMITER [ ]\n");
+  spef result;
 
-   std::string no_map = preamble + empty_flow + delimiters + units ;
-   parse_check(no_map, result);
+  std::string no_map = spefData::header;
+  parse_check(no_map, result);
 
-   std::stringstream testspef(no_map + "*NAME_MAP\n*5 MOD1/BLK2/CellA[21]\n*10 top_lvl99\n");
-   testspef.unsetf(std::ios::skipws);
-   SpefIter beg(testspef), end;
-   spefVisitor.names.clear();
-   BOOST_CHECK( phrase_parse(beg, end, spefParser, spefSkipper, result) );
-   BOOST_CHECK( (beg == end) );
+  Visitor spefVisitor;
+  parse_check(no_map + "*NAME_MAP\n*5 MOD1/BLK2/CellA[21]\n*10 top_lvl99\n",
+              spefVisitor, result);
 
-   BOOST_REQUIRE_EQUAL( 2, spefVisitor.names.size() );
-   BOOST_CHECK_EQUAL( "MOD1/BLK2/CellA[21]", spefVisitor.names[0] );
-   BOOST_CHECK_EQUAL( "top_lvl99", spefVisitor.names[1] );
+  BOOST_REQUIRE_EQUAL( 2, spefVisitor.names.size() );
+  BOOST_CHECK_EQUAL( "MOD1/BLK2/CellA[21]", spefVisitor.names[0] );
+  BOOST_CHECK_EQUAL( "top_lvl99", spefVisitor.names[1] );
 
 }
 
 BOOST_AUTO_TEST_CASE( ports ) {
 
-   spef result;
-   std::string units("*T_UNIT 1 PS\n*C_UNIT 1 PF\n*R_UNIT 1 KOHM\n*L_UNIT 1 HENRY\n");
-   std::string preamble("SPEF\n*SPEF \"IEEE 1481-1998\"\n*DESIGN \"GreatDesign\"\n");
-   preamble += "*DATE \"Fri Aug 29 02:14:00 1997\"\n*VENDOR \"MegaEDA\"\n";
-   preamble += "*PROGRAM \"Ozymandius Extractor\"\n*VERSION \"0.99\"\n";
-   std::string empty_flow("*DESIGN_FLOW\n");
-   std::string delimiters("*DIVIDER /\n*DELIMITER :\n*BUS_DELIMITER [ ]\n");
-   std::string name_map("*NAME_MAP\n*1 A/B/C\n*2 x1_12\n*3 z22[8]\n");
+  spef result;
+  std::string name_map("*NAME_MAP\n*1 A/B/C\n*2 x1_12\n*3 z22[8]\n");
 
-   std::string no_ports = preamble + empty_flow + delimiters + units + name_map;
-   std::stringstream testspef(no_ports);
-   testspef.unsetf(std::ios::skipws);
-   SpefIter beg(testspef), end;
-   spefVisitor.names.clear();
-   BOOST_CHECK( phrase_parse(beg, end, spefParser, spefSkipper, result) );
-   BOOST_CHECK( (beg == end) );
-   BOOST_CHECK_EQUAL( 0, spefVisitor.ports.size() );
+  std::string no_ports = spefData::header + name_map;
+  Visitor spefVisitorNp;
+  parse_check(no_ports, spefVisitorNp, result);
+  BOOST_CHECK_EQUAL( 0, spefVisitorNp.ports.size() );
 
-   // parse again, this time supplying some ports
-   std::stringstream with_ports(no_ports + "*PORTS\n" +
-                                "*1 I *C 3.6 0\n" +
-                                "*2 O *C 88.99 123.2\n" +
-                                "*3 B *L 0.00 *S 0.0 0.0\n");
-   with_ports.unsetf(std::ios::skipws);
-   beg = SpefIter(with_ports);
-   spefVisitor.names.clear();
-   BOOST_CHECK( phrase_parse(beg, end, spefParser, spefSkipper, result) );
-   BOOST_CHECK( (beg == end) );
-   BOOST_REQUIRE_EQUAL( 3, spefVisitor.ports.size() );
-   BOOST_CHECK_EQUAL( (std::make_pair<size_t, char>(0, 'I')), spefVisitor.ports[0] );
-   BOOST_CHECK_EQUAL( (std::make_pair<size_t, char>(1, 'O')), spefVisitor.ports[1] );
-   BOOST_CHECK_EQUAL( (std::make_pair<size_t, char>(2, 'B')), spefVisitor.ports[2] );
+  // parse again, this time supplying some ports
+  std::string with_ports(no_ports + "*PORTS\n" +
+                         "*1 I *C 3.6 0\n" +
+                         "*2 O *C 88.99 123.2\n" +
+                         "*3 B *L 0.00 *S 0.0 0.0\n");
+  Visitor spefVisitor;
+  parse_check(with_ports, spefVisitor, result);
+
+  BOOST_REQUIRE_EQUAL( 3, spefVisitor.ports.size() );
+  BOOST_CHECK_EQUAL( (std::make_pair<size_t, char>(0, 'I')), spefVisitor.ports[0] );
+  BOOST_CHECK_EQUAL( (std::make_pair<size_t, char>(1, 'O')), spefVisitor.ports[1] );
+  BOOST_CHECK_EQUAL( (std::make_pair<size_t, char>(2, 'B')), spefVisitor.ports[2] );
    
 }
