6cec2e0f3b0b026ae473ccc6534e7db9d5f0b23a
Jeff Trull <edaskel@att.net>
2014-01-21 10:50:16 -0800

    SPEF: store RC circuit elements into a Boost.Graph for further analysis

--- apps/load_spef.cpp
+++ apps/load_spef.cpp
@@ -22,9 +22,26 @@
 #define BOOST_SPIRIT_USE_PHOENIX_V3
 #include "../parser/spefparser.hpp"
 
+// Graph library for storing RC networks
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/graph_utility.hpp>
+#include <boost/property_map/property_map.hpp>
+
+// vertex (circuit node) properties are just a string with the name we got out of SPEF;
+typedef std::string vertex_property_t;
+
+// edge (circuit component) properties are the type (resistor or capacitor) with the component value
 using namespace boost::units;
+typedef quantity<si::resistance, double> resistor_edge_t;
+typedef quantity<si::capacitance, double> capacitor_edge_t;
+typedef boost::variant<resistor_edge_t, capacitor_edge_t> edge_property_t;
+
+typedef boost::adjacency_list<boost::vecS, boost::listS, boost::undirectedS,
+                              vertex_property_t, edge_property_t> CktGraph;
 
 struct Visitor {
+  Visitor() : gnd(add_vertex(g)) {}
+
   typedef size_t name_token_value_t;
   name_token_value_t name_map_entry(std::string n) {
     name_token_value_t id = names.size();
@@ -47,19 +64,52 @@ struct Visitor {
   void capacitor(name_token_value_t net, unsigned capnum,
                  name_token_value_t net_or_inst1, std::string const& node_or_pin1,
                  name_token_value_t net_or_inst2, std::string const& node_or_pin2,
-                 quantity<si::capacitance, double> value) {}
+                 quantity<si::capacitance, double> value) {
+    // store in graph as edge
+    boost::add_edge(get_vertex(net_or_inst1, node_or_pin1),
+                    get_vertex(net_or_inst2, node_or_pin2),
+                    value, g);
+  }
 
   void cgnd     (name_token_value_t net, unsigned capnum,
                  name_token_value_t net_or_inst, std::string const& node_or_pin,
-                 quantity<si::capacitance, double> value) {}
+                 quantity<si::capacitance, double> value) {
+    boost::add_edge(get_vertex(net_or_inst, node_or_pin),
+                    gnd,
+                    value, g);
+  }
 
   void resistor (name_token_value_t net, unsigned capnum,
                  name_token_value_t net_or_inst1, std::string const& node_or_pin1,
                  name_token_value_t net_or_inst2, std::string const& node_or_pin2,
-                 quantity<si::resistance, double> value) {}
+                 quantity<si::resistance, double> value) {
+    boost::add_edge(get_vertex(net_or_inst1, node_or_pin1),
+                    get_vertex(net_or_inst2, node_or_pin2),
+                    value, g);
+}
 
   std::vector<std::string> names;
   std::map<name_token_value_t, quantity<si::capacitance, double> > lumped_caps;
+
+  CktGraph g;
+
+private:
+  CktGraph::vertex_descriptor gnd;
+
+  CktGraph::vertex_descriptor get_vertex(name_token_value_t net, std::string const& node_or_pin) {
+    // return index of net/node pair for use in circuit graph
+    VertexMap::const_iterator it = vertex_desc_map.find(std::make_pair(net, node_or_pin));
+    if (it == vertex_desc_map.end()) {
+      bool found;  // it won't be
+      std::tie(it, found) = vertex_desc_map.emplace(std::make_pair(net, node_or_pin),
+                                                    add_vertex(g)); // no vertex property for now
+    }
+    return it->second;
+  }
+
+  typedef std::map<std::pair<name_token_value_t, std::string>,
+                   CktGraph::vertex_descriptor> VertexMap;
+  VertexMap vertex_desc_map;
 };
 
 
@@ -99,5 +149,7 @@ int main(int argc, char **argv) {
    for (auto lc : spefVisitor.lumped_caps) {
       cout << lc.second << " " << spefVisitor.names.at(lc.first) << endl;
    }
+
+   cout << "Parasitic graph contains " << num_vertices(spefVisitor.g) << " vertices and " << num_edges(spefVisitor.g) << " edges" << endl;
 
 }
